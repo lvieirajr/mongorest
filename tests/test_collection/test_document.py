e7bd1151cb1c4220f51620f959e4876f774c973c
Luis Antonio Vieira Junior <lvieira@lvieira.com>
2015-04-19 18:36:19 -0300

    all init tests

--- tests/test_collection/test_document.py
+++ tests/test_collection/test_document.py
@@ -1,5 +1,7 @@
 # -*- encoding: UTF-8 -*-
 
+from unittest.mock import patch
+
 from mongorest.collection import Collection, Document
 from mongorest.testcase import TestCase
 
@@ -20,3 +22,20 @@ class TestDocument(TestCase):
 
         self.assertEqual(document._fields, {'test': 'test'})
 
+    @patch('mongorest.collection.Document._validate')
+    def test_init_calls_validate(self, validate):
+        document = Document(Collection)
+
+        self.assertEqual(validate.call_count, 1)
+
+    @patch('mongorest.collection.Document._process')
+    def test_init_calls_process_if_not_processed(self, process):
+        document = Document(Collection, processed=False)
+
+        self.assertEqual(process.call_count, 1)
+
+    @patch('mongorest.collection.Document._process')
+    def test_init_does_not_call_process_if_processed(self, process):
+        document = Document(Collection, processed=True)
+
+        self.assertEqual(process.call_count, 0)
