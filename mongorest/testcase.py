13684422c15a38fdf847afccca85fdbeec937d56
Luis Antonio Vieira Junior <lvieira@lvieira.com>
2015-04-21 19:19:19 -0300

    adding a test module for each of the frameworks files and enhance testcase, not it has a client to test requests

--- mongorest/testcase.py
+++ mongorest/testcase.py
@@ -1,25 +1,33 @@
 # -*- encoding: UTF-8 -*-
 
 import logging
+
 from unittest import TestCase as BaseTestCase
+from werkzeug.test import Client
+from werkzeug.testapp import test_app
+from werkzeug.wrappers import BaseResponse
 
 from .database import db
+
+__all__ = [
+    'TestCase'
+]
 
 
 class TestCase(BaseTestCase):
 
     def __init__(self, methodName='runtest'):
         super(TestCase, self).__init__(methodName)
-
         self.maxDiff = None
+
         self.db = db
+        self.client = Client(test_app, BaseResponse)
 
         logging.disable(logging.CRITICAL)
 
     def __call__(self, *args, **kwargs):
         try:
             return super(TestCase, self).__call__(*args, **kwargs)
-
         finally:
             for collection in self.db.collection_names():
                 if collection.startswith('system'):
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,10 @@
 # -*- encoding: UTF-8 -*-
 
 from .test_collection import *
+from .test_database import *
 from .test_document import *
+from .test_resource import *
+from .test_settings import *
+from .test_testcase import *
 from .test_utils import *
+from .test_wsgi import *
