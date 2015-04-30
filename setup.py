6b61347beb638836b6d95d0e93b7b5e6815f0b2c
Luis Antonio Vieira Junior <lvieira@lvieira.com>
2015-04-30 01:15:23 -0300

    1.1.3

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ except ImportError:
 setup(
     name='mongorest',
     packages=['mongorest'],
-    version='1.1.2',
+    version='1.1.3',
     description='Easy REST APIs using MongoDB.',
     author='Luis Vieira',
     author_email='lvieira@lvieira.com',
     url='https://github.com/lvieirajr/mongorest',
-    download_url='https://github.com/lvieirajr/mongorest/tarball/1.1.2',
+    download_url='https://github.com/lvieirajr/mongorest/tarball/1.1.3',
     install_requires=['pymongo', 'werkzeug', 'six'],
     keywords=['python', 'mongodb', 'rest', 'api', 'pymongo', 'werkzeug'],
     classifiers=[
