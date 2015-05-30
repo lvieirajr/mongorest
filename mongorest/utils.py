9dad44b937085a8f77420b8c95e18fedc2f5ab6a
Luis Antonio Vieira Junior <lvieira@lvieira.com>
2015-05-30 01:47:48 -0300

    2.3.0

--- mongorest/utils.py
+++ mongorest/utils.py
@@ -2,11 +2,9 @@
 from __future__ import absolute_import, unicode_literals
 
 from bson.json_util import dumps, loads
-from os.path import abspath
 
 __all__ = [
     'deserialize',
-    'load_module',
     'serialize',
 ]
 
@@ -16,18 +14,6 @@ def deserialize(to_deserialize):
     Deserializes a string into a PyMongo BSON
     """
     return loads(to_deserialize)
-
-
-def load_module(module):
-    try:
-        from importlib import import_module
-        return import_module(module)
-    except ImportError:
-        from imp import load_source
-        return load_source(
-            'module',
-            abspath('{0}.py'.format(module.replace('.', '/')))
-        )
 
 
 def serialize(to_serialize):
