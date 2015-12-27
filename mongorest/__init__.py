9247e6701fe2c9bb0d430cc55a55788781db09ab
Luis Antonio Vieira Junior <lvieira@lvieira.com>
2015-12-27 16:05:32 -0200

    removing app for now

--- mongorest/__init__.py
+++ mongorest/__init__.py
@@ -1,7 +1,6 @@
 # -*- encoding: UTF-8 -*-
 from __future__ import absolute_import, unicode_literals
 
-from .app import App
 from .collection import Collection
 from .wrappers import Request, Response
 
