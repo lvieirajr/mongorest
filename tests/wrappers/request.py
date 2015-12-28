e13f8d00126eb01f2c3f2fd64a2dc94a7b7b5fe0
Luis Antonio Vieira Junior <lvieira@lvieira.com>
2015-12-28 18:42:40 -0200

    Closing issue #7. Request and Response wrappers created.

--- tests/wrappers/request.py
+++ tests/wrappers/request.py
@@ -1,11 +1,12 @@
 # -*- encoding: UTF-8 -*-
 from __future__ import absolute_import, unicode_literals
 
-import io
 try:
     from collections import OrderedDict
 except:
     from ordereddict import OrderedDict
+
+from werkzeug.test import create_environ
 
 from mongorest.testcase import TestCase
 from mongorest.wrappers import Request
@@ -15,25 +16,27 @@ class TestRequest(TestCase):
 
     def test_init_makes_args_become_a_dict_with_ordered_values_for_each_key(self):
         request1 = Request(
-            environ={
-                'QUERY_STRING': 'test={"a":2,"b":3}',
-                'wsgi.input': io.BytesIO(b''),
-            }
+            create_environ(method='POST', query_string='test={"a":1,"b":2}')
         )
 
         request2 = Request(
-            environ={
-                'QUERY_STRING': 'test={"b":2,"a":3}',
-                'wsgi.input': io.BytesIO(b''),
-            }
+            create_environ(method='POST', query_string='test={"b":2,"a":1}')
         )
 
         self.assertEqual(
-            request1.args,
-            {'test': OrderedDict([('a', 2), ('b', 3)])}
+            request1.args, {'test': OrderedDict([('a', 1), ('b', 2)])}
         )
 
         self.assertEqual(
-            request2.args,
-            {'test': OrderedDict([('b', 2), ('a', 3)])}
+            request2.args, {'test': OrderedDict([('b', 2), ('a', 1)])}
         )
+
+    def test_init_makes_form_a_dict_with_decoded_and_deserialized_form_data(self):
+        request = Request(create_environ(data={'a': 1, 'b': 2}))
+
+        self.assertEqual(request.form, {'a': 1, 'b': 2})
+
+    def test_init_makes_json_a_dict_with_decoded_and_deserialized_data(self):
+        request = Request(create_environ(data='{"a":1,"b":2}'))
+
+        self.assertEqual(request.json, {'a': 1, 'b': 2})
