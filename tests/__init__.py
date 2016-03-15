5739e985ac1c2cec6992307e7c24e62c9230585d
Luis Antonio Vieira Junior <lvieira@lvieira.com>
2016-03-15 19:41:10 -0300

    removing document

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,7 +4,6 @@ from __future__ import absolute_import, unicode_literals
 from .collection import *
 from .database import *
 from .decorators import *
-from .document import *
 from .errors import *
 from .middlewares import *
 from .resource import *
