fbfee357beda8e37160c1f5d5aa631d5bc7d6667
Luis Antonio Vieira Junior <lvieira@lvieira.com>
2016-03-15 19:40:51 -0300

    finished transfering tests from document to collection

--- tests/collection/collection.py
+++ tests/collection/collection.py
@@ -72,7 +72,7 @@ class TestCollection(TestCase):
         self.assertEqual(after_validation_succeeded.call_count, 1)
 
     @patch('mongorest.collection.Collection.after_validation_succeeded')
-    def test_init_does_not_call_after_validation_succeeded_if_validation_succeeds_and_after_validation_returns_something(self, after_validation_succeeded):
+    def test_init_doesnt_call_after_validation_succeeded_if_validation_succeeds_and_after_validation_returns_something(self, after_validation_succeeded):
         self.collection.after_validation = lambda self: True
 
         self.collection()
@@ -297,6 +297,64 @@ class TestCollection(TestCase):
                 'document': document,
             }
         )
+
+    def test_update_returns_errors_if_document_has_no_id(self):
+        errors = Collection().update()
+
+        self.assertEqual(
+            errors,
+            {
+                'error_code': 11,
+                'error_type': 'UnidentifiedDocumentError',
+                'error_message': 'The given document from collection \'Collection\' has no _id.',
+                'collection': 'Collection',
+                'document': {},
+            }
+        )
+
+    def test_update_returns_errors_if_error_ocurred_during_save(self):
+        Collection.collection.create_index('test', unique=True)
+        Collection.insert_one({'test': 'test1'})
+
+        _id = Collection.insert_one({'test': 'test2'})
+
+        errors = Collection({'_id': _id, 'test': 'test1'}).update()
+
+        errors.pop('error_message')
+        self.assertEqual(
+            errors,
+            {
+                'error_code': 0,
+                'error_type': 'PyMongoError',
+                'operation': 'update',
+                'collection': 'Collection',
+                'document': {'_id': _id, 'test': 'test1'}
+            }
+        )
+
+        Collection.drop_index('test_1')
+
+    def test_update_returns_errors_if_document_not_found(self):
+        errors = Collection({'_id': 1}).update()
+
+        self.assertEqual(
+            errors,
+            {
+                'error_code': 12,
+                'error_type': 'DocumentNotFoundError',
+                'error_message': '1 is not a valid _id for a document from collection \'Collection\'.',
+                'collection': 'Collection',
+                '_id': 1,
+            }
+        )
+
+    @patch('mongorest.collection.Collection.after_update')
+    def test_update_updates_calls_cascade_and_returns_updated_document_if_document_is_valid(self, after_update):
+        _id = Collection.insert_one({'test': 'test1'})
+        updated = Collection({'_id': _id, 'test': 'test2'}).update()
+
+        self.assertEqual(updated, {'_id': _id, 'test': 'test2'})
+        self.assertEqual(after_update.call_count, 1)
 
     # delete
     def test_delete_is_decorated_with_serializable(self):
--- tests/document/document.py
+++ tests/document/document.py
@@ -13,59 +13,8 @@
 #
 #
 # class TestDocument(TestCase):
-#     def test_update_returns_errors_if_document_has_no_id(self):
-#         errors = Document(Collection).update()
 #
-#         self.assertEqual(
-#             errors,
-#             {
-#                 'error_code': 11,
-#                 'error_type': 'UnidentifiedDocumentError',
-#                 'error_message': 'The given document from collection \'Collection\' has no _id.',
-#                 'collection': 'Collection',
-#                 'document': {},
-#             }
-#         )
 #
-#     def test_update_returns_errors_if_error_ocurred_during_save(self):
-#         Collection.collection.create_index('test', unique=True)
-#         Collection.insert_one({'test': 'test1'})
-#         _id = Collection.insert_one({'test': 'test2'})
+
 #
-#         errors = Document(Collection, {'_id': _id, 'test': 'test1'}).update()
-#
-#         errors.pop('error_message')
-#         self.assertEqual(
-#             errors,
-#             {
-#                 'error_code': 0,
-#                 'error_type': 'PyMongoError',
-#                 'operation': 'update',
-#                 'collection': 'Collection',
-#                 'document': {'_id': _id, 'test': 'test1'}
-#             }
-#         )
-#
-#         Collection.drop_index('test_1')
-#
-#     def test_update_returns_errors_if_document_not_found(self):
-#         errors = Document(Collection, {'_id': 1}).update()
-#
-#         self.assertEqual(
-#             errors,
-#             {
-#                 'error_code': 12,
-#                 'error_type': 'DocumentNotFoundError',
-#                 'error_message': '1 is not a valid _id for a document from collection \'Collection\'.',
-#                 'collection': 'Collection',
-#                 '_id': 1,
-#             }
-#         )
-#
-#     @patch('mongorest.collection.Collection.cascade_update')
-#     def test_update_updates_calls_cascade_and_returns_updated_document_if_document_is_valid(self, cascade):
-#         _id = Collection.insert_one({'test': 'test1'})
-#         updated = Document(Collection, {'_id': _id, 'test': 'test2'}).update()
-#
-#         self.assertEqual(updated, {'_id': _id, 'test': 'test2'})
-#         self.assertEqual(cascade.call_count, 1)
+
