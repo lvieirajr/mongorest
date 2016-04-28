1f0d1327f27f3697ccb4f519114f599eb3e3669c
Luis Antonio Vieira Junior <lvieira@lvieira.com>
2016-04-28 15:28:16 -0300

    fixing deserialize

--- mongorest/utils.py
+++ mongorest/utils.py
@@ -18,10 +18,12 @@ def deserialize(to_deserialize, *args, **kwargs):
     Deserializes a string into a PyMongo BSON
     """
     if isinstance(to_deserialize, string_types):
-        if re.match('[0-9a-f]{24}', to_deserialize):
+        if re.match('^[0-9a-f]{24}$', to_deserialize):
             return ObjectId(to_deserialize)
-
-        return bson_loads(to_deserialize, *args, **kwargs)
+        try:
+            return bson_loads(to_deserialize, *args, **kwargs)
+        except:
+            return bson_loads(bson_dumps(to_deserialize), *args, **kwargs)
     else:
         return bson_loads(bson_dumps(to_deserialize), *args, **kwargs)
 
